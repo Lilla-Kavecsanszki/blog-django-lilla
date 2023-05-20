579c2d1d2704515073975a5ade1aa378f8c0c291
Lilla-Kavecsanszki <>
2023-05-20 15:45:31 +0000

    Create and the Post detail view

--- blog_lilla/urls.py
+++ blog_lilla/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("", include("blogdjangolilla.urls"), name="blog-urls"),
     path('summernote/', include('django_summernote.urls')),
 ]
