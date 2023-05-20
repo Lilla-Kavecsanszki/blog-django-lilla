92ac812eaf24e94332d252933f3f81b43482159c
Lilla-Kavecsanszki <>
2023-05-20 16:25:11 +0000

    Start adding the authorisation process

--- blog_lilla/urls.py
+++ blog_lilla/urls.py
@@ -20,4 +20,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path("", include("blogdjangolilla.urls"), name="blog-urls"),
     path('summernote/', include('django_summernote.urls')),
+    path("accounts/", include("allauth.urls")),
 ]
