c2a0249f24ae37eadcf36a426f5fbddd3a4ac10e
Lilla-Kavecsanszki <>
2023-05-25 17:18:55 +0000

    Deployment commit

--- blog_lilla/settings.py
+++ blog_lilla/settings.py
@@ -28,7 +28,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["blog_lilla.herokuapp.com",
                  "localhost",
@@ -36,6 +36,7 @@ ALLOWED_HOSTS = ["blog_lilla.herokuapp.com",
                  '8000-lilla-kavecsanszki-blog-l6wzq476do.us2.codeanyapp.com',
                  "blogdjangolilla.herokuapp.com"]
 
+X_FRAME_OPTIONS = 'SAMEORIGIN'
 
 # Application definition
 
